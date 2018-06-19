4ed88aed431ccff6d7af1c19b5f64f8c3dd951ef
cagri <bencagri>
2018-06-19 14:51:28 +0300

    enable multiple lines

--- src/Selectors.php
+++ src/Selectors.php
@@ -2,16 +2,13 @@
 
 namespace DngoIO\CoverCreator;
 
-use DngoIO\CoverCreator\Exception\InvalidTypeException;
-use DngoIO\CoverCreator\Selectors\SelectorValidator;
-
 class Selectors
 {
 
     /**
      * @var array
      */
-    public $selectorRules= [
+    public static $selectorRules= [
         'font-size' => ['integer'], //px
         'font-type' => ['file'], //path of ttf file on server
         'text-color' => ['rgb'], // RGB as array
@@ -19,21 +16,5 @@ class Selectors
         'top' => ['integer'],
         'background-url' => ['file'] //path of the png
     ];
-
-    /**
-     * Selectors constructor.
-     * @param array $selectors
-     * @throws InvalidTypeException
-     * @throws \SimpleValidator\SimpleValidatorException
-     */
-    public function __construct(array $selectors)
-    {
-        $validator = SelectorValidator::validate($selectors,$this->selectorRules);
-        if ($validator->isSuccess() == false) {
-            throw new InvalidTypeException(implode(',',$validator->getErrors()));
-        }
-
-        return $selectors;
-    }
 
 }
